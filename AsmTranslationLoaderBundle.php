73cc06092d0b44422ec494d081595a89f2d793f2
Marc Aschmann <marc@aschmann.org>
2014-02-02 20:28:21 +0100

    fixed merge error

--- AsmTranslationLoaderBundle.php
+++ AsmTranslationLoaderBundle.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the AsmTranslationLoaderBundle package.
+ *
+ * (c) Marc Aschmann <maschmann@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * @namespace Asm\TranslationLoaderBundle
  */
@@ -6,7 +15,7 @@ namespace Asm\TranslationLoaderBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Asm\TranslationLoaderBundle\DependencyInjection\Compiler\TranslationLoaderPass;
+use Asm\TranslationLoaderBundle\DependencyInjection\Compiler\AddResourcePass;
 
 /**
  * Class AsmTranslationLoaderBundle
